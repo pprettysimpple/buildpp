2bee33b3ff532a0a18ed16fc60eee1ecd4c1e202
Kirill Andriianov <andrianov.kirill2010@yandex.ru>
2025-12-22 10:13:18 +0100

    Small change to how 01_simple looks

--- how_to/01_simple/build.cpp
+++ how_to/01_simple/build.cpp
@@ -2,12 +2,10 @@
 #include "buildpp.h"
 
 void configure(Build* b) {
-    b->dump_compile_commands = true;
-    
-    auto main = b->addExe({.name = "main", .desc = "My simple binary artefact"}, {"main.cpp"});
+    auto main = b->addExe({.name = "main", .desc = "My simple executable"}, {"main.cpp"});
     b->installExe(main);
     b->addRunExe(main, {.name = "run", .desc = "Run the main executable", .args = b->cli_args});
 
-    auto libmain = b->addLib({.name = "main", .desc = "My simple binary artefact"/*, .link = StaticLinkTool{"gcc-ar"}*/}, {"main.cpp"});
+    auto libmain = b->addLib({.name = "main", .desc = "My simple library"}, {"main.cpp"});
     b->installLib(libmain);
 }
